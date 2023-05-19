bccf8b9a17f6dfab5e059dff8a12c777308d281b
K-MHOON <azmria117@gmail.com>
2023-05-19 22:32:41 +0900

    Initial commit

---
+++ src/main/java/com/kmhoon/msa/KmhoonMsaApplication.java
@@ -0,0 +1,13 @@
+package com.kmhoon.msa;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class KmhoonMsaApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(KmhoonMsaApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/kmhoon/msa/KmhoonMsaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.kmhoon.msa;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class KmhoonMsaApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
