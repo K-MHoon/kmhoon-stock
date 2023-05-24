c600e26bb6fce58f2e71a8d2c2b676642ee7081a
K-MHOON <azmria117@gmail.com>
2023-05-25 00:12:03 +0900

    feat : 국제화 설정 추가

---
+++ licensing-service/src/main/java/com/kmhoon/licensingservice/configuration/LocaleConfig.java
@@ -0,0 +1,28 @@
+package com.kmhoon.licensingservice.configuration;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.support.ResourceBundleMessageSource;
+import org.springframework.web.servlet.LocaleResolver;
+import org.springframework.web.servlet.i18n.SessionLocaleResolver;
+
+import java.util.Locale;
+
+@Configuration
+public class LocaleConfig {
+
+    @Bean
+    public LocaleResolver localeResolver() {
+        SessionLocaleResolver localeResolver = new SessionLocaleResolver();
+        localeResolver.setDefaultLocale(Locale.KOREA);
+        return localeResolver;
+    }
+
+    @Bean
+    public ResourceBundleMessageSource messageSource() {
+        ResourceBundleMessageSource messageSource = new ResourceBundleMessageSource();
+        messageSource.setUseCodeAsDefaultMessage(true); // 메시지가 발견되지 않아도 에러를 던지지 않는다.
+        messageSource.setBasenames("messages"); // 소스 파일의 기본 이름 설정
+        return messageSource;
+    }
+}
