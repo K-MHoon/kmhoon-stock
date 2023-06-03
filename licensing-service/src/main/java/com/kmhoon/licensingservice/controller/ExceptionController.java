852abee1c3d4b5b156fdec7442ddd2be37ad58df
K-MHOON <azmria117@gmail.com>
2023-06-03 11:56:33 +0900

    feat[license] : 예외 처리를 위한 ExceptionController 추가

---
+++ licensing-service/src/main/java/com/kmhoon/licensingservice/controller/ExceptionController.java
@@ -0,0 +1,33 @@
+package com.kmhoon.licensingservice.controller;
+
+import com.kmhoon.licensingservice.model.utils.ErrorMessage;
+import com.kmhoon.licensingservice.model.utils.ResponseWrapper;
+import com.kmhoon.licensingservice.model.utils.RestErrorList;
+import jakarta.servlet.http.HttpServletRequest;
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.ResponseStatus;
+import org.springframework.web.bind.annotation.RestControllerAdvice;
+import org.springframework.web.servlet.config.annotation.EnableWebMvc;
+import org.springframework.web.servlet.mvc.method.annotation.ResponseEntityExceptionHandler;
+
+import static java.util.Collections.singletonMap;
+
+@RestControllerAdvice
+@EnableWebMvc
+public class ExceptionController extends ResponseEntityExceptionHandler {
+
+    @ExceptionHandler(value = { Exception.class })
+    @ResponseStatus(HttpStatus.OK)
+    public ResponseWrapper handleException(HttpServletRequest request, ResponseWrapper responseWrapper) {
+        return responseWrapper;
+    }
+
+    @ExceptionHandler(RuntimeException.class)
+    @ResponseStatus(HttpStatus.OK)
+    public ResponseWrapper handleIOException(HttpServletRequest request, RuntimeException e) {
+        RestErrorList errorList = new RestErrorList(HttpStatus.NOT_ACCEPTABLE, new ErrorMessage(e.getMessage(), e.getMessage()));
+        ResponseWrapper responseWrapper = new ResponseWrapper(null, singletonMap("status", HttpStatus.NOT_ACCEPTABLE), errorList);
+        return responseWrapper;
+    }
+}
