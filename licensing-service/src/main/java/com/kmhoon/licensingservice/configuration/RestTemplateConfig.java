c803b68d1264ce703a456fb29cbf9174f37d3bc2
K-MHOON <azmria117@gmail.com>
2023-06-07 01:08:59 +0900

    feat[license] : 라이센싱 서비스 RestTemplate 인터셉터 추가
    - 항상 헤더에 ID 추가하도록 설정

--- licensing-service/src/main/java/com/kmhoon/licensingservice/configuration/RestTemplateConfig.java
+++ licensing-service/src/main/java/com/kmhoon/licensingservice/configuration/RestTemplateConfig.java
@@ -1,9 +1,14 @@
 package com.kmhoon.licensingservice.configuration;
 
+import com.kmhoon.licensingservice.utils.UserContextInterceptor;
 import org.springframework.cloud.client.loadbalancer.LoadBalanced;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.http.client.ClientHttpRequestInterceptor;
 import org.springframework.web.client.RestTemplate;
+
+import java.util.Collections;
+import java.util.List;
 
 @Configuration
 public class RestTemplateConfig {
@@ -15,6 +20,15 @@ public class RestTemplateConfig {
     @LoadBalanced
     @Bean
     public RestTemplate getRestTemplate() {
-        return new RestTemplate();
+        RestTemplate restTemplate = new RestTemplate();
+        List<ClientHttpRequestInterceptor> interceptors =
+                restTemplate.getInterceptors();
+        if(interceptors == null) {
+            restTemplate.setInterceptors(Collections.singletonList(new UserContextInterceptor()));
+        } else {
+            interceptors.add(new UserContextInterceptor());
+            restTemplate.setInterceptors(interceptors);
+        }
+        return restTemplate;
     }
 }
