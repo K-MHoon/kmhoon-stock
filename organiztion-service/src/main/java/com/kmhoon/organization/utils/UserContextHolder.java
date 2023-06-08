b9edf95d87ace98ed817b0d8de1beff1dacee6d2
K-MHOON <azmria117@gmail.com>
2023-06-08 21:39:15 +0900

    feat[organization] : 조직 서비스 상관관계 ID 필터 추가

---
+++ organiztion-service/src/main/java/com/kmhoon/organization/utils/UserContextHolder.java
@@ -0,0 +1,27 @@
+package com.kmhoon.organization.utils;
+
+import org.springframework.util.Assert;
+
+public class UserContextHolder {
+
+    private static final ThreadLocal<UserContext> userContext =  new ThreadLocal<UserContext>();
+
+    public static final UserContext getContext() {
+        UserContext context = userContext.get();
+
+        if(context == null) {
+            context = createEmptyContext();
+            setContext(context);
+        }
+        return userContext.get();
+    }
+
+    public static final void setContext(UserContext context) {
+        Assert.notNull(context, "UserContext 인스턴스는 널 값이면 안됩니다.");
+        userContext.set(context);
+    }
+
+    public static final UserContext createEmptyContext() {
+        return new UserContext();
+    }
+}
