614ac7f51a1a7f76e6f0e00997862e219806947d
K-MHOON <azmria117@gmail.com>
2023-06-09 21:55:26 +0900

    feat[organization] : keycloak adapter 의존성 추가 및 컨트롤러 Role 세팅

--- organiztion-service/src/main/java/com/kmhoon/organization/controller/OrganizationController.java
+++ organiztion-service/src/main/java/com/kmhoon/organization/controller/OrganizationController.java
@@ -3,6 +3,7 @@ package com.kmhoon.organization.controller;
 import com.kmhoon.organization.model.dto.OrganizationDto;
 import com.kmhoon.organization.model.entity.Organization;
 import com.kmhoon.organization.service.OrganizationService;
+import jakarta.annotation.security.RolesAllowed;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
@@ -16,23 +17,27 @@ public class OrganizationController {
     private final OrganizationService service;
 
     @GetMapping("/{organizationId}")
+    @RolesAllowed({"ADMIN", "USER"})
     public ResponseEntity<Organization> getOrganization(@PathVariable("organizationId") String organizationId) {
         return ResponseEntity.ok(service.findById(organizationId));
     }
 
     @PutMapping
     @ResponseStatus(HttpStatus.OK)
+    @RolesAllowed({"ADMIN", "USER"})
     public void updateOrganization(@RequestBody OrganizationDto organizationDto) {
         service.update(organizationDto);
     }
 
     @PostMapping
+    @RolesAllowed({"ADMIN", "USER"})
     public ResponseEntity<Organization> saveOrganization(@RequestBody OrganizationDto organizationDto) {
         return ResponseEntity.ok(service.create(organizationDto));
     }
 
     @DeleteMapping("/{organizationId}")
     @ResponseStatus(HttpStatus.NO_CONTENT)
+    @RolesAllowed("ADMIN")
     public void deleteOrganization(@PathVariable("organizationId") String organizationId) {
         service.delete(organizationId);
     }
