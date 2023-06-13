5c4bb215a3f4c8ce6e11909266fbcf74706777d3
K-MHOON <azmria117@gmail.com>
2023-06-13 22:24:02 +0900

    feat[organization] : Spring cloud Stream Kafka 의존성 추가 및 UserContext ThreadLocal 설정

--- organiztion-service/src/main/java/com/kmhoon/organization/utils/UserContext.java
+++ organiztion-service/src/main/java/com/kmhoon/organization/utils/UserContext.java
@@ -2,11 +2,10 @@ package com.kmhoon.organization.utils;
 
 import lombok.Getter;
 import lombok.Setter;
+import org.springframework.http.HttpHeaders;
 import org.springframework.stereotype.Component;
 
 @Component
-@Getter
-@Setter
 public class UserContext {
 
     public static final String CORRELATION_ID = "tmx-correlation-id";
@@ -14,8 +13,28 @@ public class UserContext {
     public static final String USER_ID = "tmx-user-id";
     public static final String ORGANIZATION_ID = "tmx-organization-id";
 
-    private String correlationId = "";
-    private String authToken = "";
-    private String userId = "";
-    private String organizationId = "";
+    private static final ThreadLocal<String> correlationId= new ThreadLocal<>();
+    private static final ThreadLocal<String> authToken= new ThreadLocal<>();
+    private static final ThreadLocal<String> userId = new ThreadLocal<>();
+    private static final ThreadLocal<String> orgId = new ThreadLocal<>();
+
+
+    public static String getCorrelationId() { return correlationId.get(); }
+    public static void setCorrelationId(String cid) {correlationId.set(cid);}
+
+    public static String getAuthToken() { return authToken.get(); }
+    public static void setAuthToken(String aToken) {authToken.set(aToken);}
+
+    public static String getUserId() { return userId.get(); }
+    public static void setUserId(String aUser) {userId.set(aUser);}
+
+    public static String getOrgId() { return orgId.get(); }
+    public static void setOrgId(String aOrg) {orgId.set(aOrg);}
+
+    public static HttpHeaders getHttpHeaders(){
+        HttpHeaders httpHeaders = new HttpHeaders();
+        httpHeaders.set(CORRELATION_ID, getCorrelationId());
+
+        return httpHeaders;
+    }
 }
