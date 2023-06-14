816ff9b3fdf39f215a4ea0fca926cd03f9912866
K-MHOON <azmria117@gmail.com>
2023-06-14 22:12:23 +0900

    feat[organization] : 조직 서비스 기능 마다 큐로 데이터 전송되도록 기능 추가

--- organiztion-service/src/main/java/com/kmhoon/organization/service/OrganizationService.java
+++ organiztion-service/src/main/java/com/kmhoon/organization/service/OrganizationService.java
@@ -1,8 +1,10 @@
 package com.kmhoon.organization.service;
 
+import com.kmhoon.organization.events.source.SimpleSourceBean;
 import com.kmhoon.organization.model.dto.OrganizationDto;
 import com.kmhoon.organization.model.entity.Organization;
 import com.kmhoon.organization.repository.OrganizationRepository;
+import com.kmhoon.organization.utils.ActionEnum;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
@@ -15,10 +17,14 @@ public class OrganizationService {
 
     private final OrganizationRepository repository;
 
+    private final SimpleSourceBean sourceBean;
+
 
     @Transactional(readOnly = true)
     public Organization findById(String organizationId) {
-        return repository.findById(organizationId).orElse(null);
+        Organization organization = repository.findById(organizationId).orElse(null);
+        sourceBean.publishOrganizationChange(ActionEnum.GET, organizationId);
+        return organization;
     }
 
     @Transactional
@@ -30,7 +36,9 @@ public class OrganizationService {
                 .contactPhone(organizationDto.getContactPhone())
                 .contactEmail(organizationDto.getContactEmail())
                 .build();
-        return repository.save(organization);
+        Organization savedOrganization = repository.save(organization);
+        sourceBean.publishOrganizationChange(ActionEnum.CREATED, savedOrganization.getId());
+        return savedOrganization;
     }
 
     @Transactional
@@ -42,11 +50,13 @@ public class OrganizationService {
                 .contactPhone(organizationDto.getContactPhone())
                 .contactEmail(organizationDto.getContactEmail())
                 .build();
+        sourceBean.publishOrganizationChange(ActionEnum.UPDATED, organization.getId());
         repository.save(organization);
     }
 
     @Transactional
     public void delete(String organizationId) {
         repository.deleteById(organizationId);
+        sourceBean.publishOrganizationChange(ActionEnum.DELETED, organizationId);
     }
 }
