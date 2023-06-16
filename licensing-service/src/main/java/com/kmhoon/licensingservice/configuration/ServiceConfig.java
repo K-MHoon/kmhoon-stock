52e5c758b5749c4eb2611d9b53473261b7ee5fee
K-MHOON <azmria117@gmail.com>
2023-06-16 20:58:51 +0900

    feat[license] : 레디스 설정 추가

--- licensing-service/src/main/java/com/kmhoon/licensingservice/configuration/ServiceConfig.java
+++ licensing-service/src/main/java/com/kmhoon/licensingservice/configuration/ServiceConfig.java
@@ -1,14 +1,19 @@
 package com.kmhoon.licensingservice.configuration;
 
 import lombok.Getter;
-import lombok.Setter;
-import org.springframework.boot.context.properties.ConfigurationProperties;
-import org.springframework.context.annotation.Configuration;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.stereotype.Component;
 
-@Configuration
-@ConfigurationProperties(prefix = "example")
+@Component
 @Getter
-@Setter
 public class ServiceConfig {
-    private String property;
+
+    @Value("${example.property}")
+    private String exampleProperty;
+
+    @Value("${redis.server}")
+    private String redisServer="";
+
+    @Value("${redis.port}")
+    private String redisPort="";
 }
